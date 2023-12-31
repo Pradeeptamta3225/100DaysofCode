6a41a837d2d43a02634fb742dfd140608eb07a56
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-12-31 07:42:20 +0530

    complete digital clock

--- script.js
+++ script.js
@@ -16,12 +16,22 @@
 // updateClock();
 
 let time = document.getElementById("timeDisplay");
+// let days = document.getElementById("dayDate");
 
+function clock(){
+    let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let today = new Date();
+    
+    document.getElementById("dayDate").innerHTML = (daysOfWeek[today.getDay()] + " " +
+     today.getDate() + " " + monthsName[today.getMonth()] + " " + today.getFullYear());
+}let inter = setInterval(clock,400);
 
 
 setInterval(() => {
+    
     let d = new Date();
-    time.innerHTML = d.toLocaleTimeString();
+    timeDisplay.innerHTML = d.toLocaleTimeString();
 },1000);
 
 
