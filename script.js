cda2b94f0b73b1ad9b0222ec125520dc4106336f
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2024-01-02 08:31:44 +0530

    fix:some code

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// function updateClock() {
-
-//     const now = new Date();
-//     const hours = now.getHours().toString().padStart(2,0);
-//     const minutes = now.getMinutes().toString().padStart(2,0);
-//     const second = now.getSeconds().toString().padStart(2,0); 
-
-//     const timeScreen = `${hours}:${minutes}:${second}`;
-
-//     document.getElementById("time").innerText = timeScreen;
-// }
-
-// setTimeout(updateClock,1000);
-
-
-// updateClock();
-
-let time = document.getElementById("timeDisplay");
-// let days = document.getElementById("dayDate");
 
 function clock(){
     let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
