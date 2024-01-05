b3303e926ead4f62be1a9ff183538aa0f01fa982
Pradeep Tamta <118155647+Pradeeptamta3225@users.noreply.github.com>
2024-01-05 09:49:00 +0530

    fix: some issue

---
+++ Image-Slider/script.js
@@ -0,0 +1,27 @@
+
+let scrollBar = document.querySelector(".gallry");
+let nextBtn = document.getElementById("nextBtn");
+let backBtn = document.getElementById("backBtn");
+
+
+
+// scrollBar.addEventListener("wheel",(e) =>{
+//     e.preventDefault();
+//     scrollBar.scrollLeft += e.deltaY;
+//     scrollBar.style.scrollBehavior = "auto";
+// })
+
+
+
+nextBtn.addEventListener("click",() =>{
+    scrollBar.scrollLeft += 900;
+    scrollBar.style.scrollBehavior = "smooth";
+    
+})
+
+backBtn.addEventListener("click",() =>{
+    scrollBar.scrollLeft -= 900;
+    scrollBar.style.scrollBehavior = "smooth";
+
+})
+
