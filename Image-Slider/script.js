ae7d4e979a622a06d616423c18818dc9aad22743
Pradeep Tamta <118155647+Pradeeptamta3225@users.noreply.github.com>
2024-02-05 09:24:20 +0530

    Developed Image-slider final

--- Image-Slider/script.js
+++ Image-Slider/script.js
@@ -3,12 +3,6 @@ let scrollBar = document.querySelector(".gallry");
 let nextBtn = document.getElementById("nextBtn");
 let backBtn = document.getElementById("backBtn");
 
-// scrollBar.addEventListener("wheel",(e) =>{
-//     e.preventDefault();
-//     scrollBar.scrollLeft += e.deltaY;
-//     scrollBar.style.scrollBehavior = "auto";
-    
-// })
 
 
 nextBtn.addEventListener("click",() =>{
