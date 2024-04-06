9af26f7ebd2e2cbd8106d51ad0007b0a474c4eea
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-06 14:39:14 +0530

    change some code

--- pinterest/script.js
+++ pinterest/script.js
@@ -5,7 +5,11 @@ var arr = [
     {name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
     {name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     {name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "car image", image: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FyfGVufDB8fDB8fHww"},
+    {name: "ice image", image: "https://plus.unsplash.com/premium_photo-1670963965022-59c9ac468c18?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FyfGVufDB8fDB8fHww"},
+    {name: "ice image", image: "https://images.unsplash.com/photo-1518780664697-55e3ad937233?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGhvbWV8ZW58MHx8MHx8fDA%3D"},
+    {name: "ice image", image: "https://images.unsplash.com/photo-1540039906769-84cf3d448bc1?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3VuZmxvd2VyfGVufDB8fDB8fHww"},
 ]
 
 
@@ -26,15 +30,33 @@ function showImage() {
 
 showImage();
 
+function SearchHandle(){
+      const input = document.querySelector("#searchinput");
+      const overLay =   document.querySelector(".overlay");
+      const searchData = document.querySelector(".searchdata")
+    
 
-function searchHandleBar() {
-    document.querySelector("#searchinput").addEventListener("focus" , function() {
-        document.querySelector(".overlay").style.display = "block"
+    input.addEventListener("focus", function(){
+      overLay.style.display = "block";
     })
 
-    document.querySelector("#searchinput").addEventListener("blur" , function() {
-        document.querySelector(".overlay").style.display = "none"
+    input.addEventListener("blur", function(){
+       overLay.style.display = "none";
+    })
+
+    input.addEventListener("input", function(){
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        let clutter = "";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+        })
+
+         searchData.style.display = "block";
+         searchData.innerHTML = clutter;
     })
 }
 
-searchHandleBar();
+SearchHandle();
