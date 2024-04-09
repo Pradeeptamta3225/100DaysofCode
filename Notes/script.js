8a5ee07d19e32d689e4148b88cb54dbce00e3837
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-09 23:45:25 +0530

    project complete

--- Notes/script.js
+++ Notes/script.js
@@ -1,2 +1,44 @@
 
-const notes = document.querySelector(".notes");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+createBtn.addEventListener("click",() =>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "/100DaysofCode/Notes/delete-bin-5-line.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach( note => {
+            note.onekeyup = function(){
+                updateStorage()
+            }
+        })
+    }
+})
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+document.addEventListener("keydown", event =>{
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
